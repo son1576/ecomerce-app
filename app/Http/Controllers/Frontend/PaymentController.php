89647d756e85171ae60304bda95f861ef5b0e9aa
TuanHung1995 <tuanhungg1995@gmail.com>
2025-02-04 22:26:04 +0700

    store order and related to db

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\GeneralSetting;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use App\Models\PaypalSetting;
+use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Srmklive\PayPal\Services\Paypal as PayPalClient;
 use Cart;
+use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
@@ -48,6 +54,58 @@ class PaymentController extends Controller
             'validate_ssl'   =>  true,
         ];
         return $config;
+    }
+
+    public function storeOrder($paymentMethod, $paymentStatus, $transactionId, $paidAmount, $paidCurrencyName)
+    {
+        $setting = GeneralSetting::first();
+
+        $order = new Order();
+        $order->invocie_id = rand(1, 999999);
+        $order->user_id = Auth::user()->id;
+        $order->sub_total = getCartTotal();
+        $order->amount =  getFinalPayableAmount();
+        $order->currency_name = $setting->currency_name;
+        $order->currency_icon = $setting->currency_icon;
+        $order->product_qty = Cart::content()->count();
+        $order->payment_method = $paymentMethod;
+        $order->payment_status = $paymentStatus;
+        $order->order_address = json_encode(Session::get('address'));
+        $order->shpping_method = json_encode(Session::get('shipping_method'));
+        $order->coupon = json_encode(Session::get('coupon'));
+        $order->order_status = 'pending';
+        $order->save();
+
+        // store order products
+        foreach(Cart::content() as $item){
+            $product = Product::find($item->id);
+            $orderProduct = new OrderProduct();
+            $orderProduct->order_id = $order->id;
+            $orderProduct->product_id = $product->id;
+            $orderProduct->vendor_id = $product->vendor_id;
+            $orderProduct->product_name = $product->name;
+            $orderProduct->variants = json_encode($item->options->variants);
+            $orderProduct->variant_total = $item->options->variants_total;
+            $orderProduct->unit_price = $item->price;
+            $orderProduct->qty = $item->qty;
+            $orderProduct->save();
+
+            // update product quantity
+            $updatedQty = ($product->qty - $item->qty);
+            $product->qty = $updatedQty;
+            $product->save();
+        }
+
+        // store transaction details
+        $transaction = new Transaction();
+        $transaction->order_id = $order->id;
+        $transaction->transaction_id = $transactionId;
+        $transaction->payment_method = $paymentMethod;
+        $transaction->amount = getFinalPayableAmount();
+        $transaction->amount_real_currency = $paidAmount;
+        $transaction->amount_real_currency_name = $paidCurrencyName;
+        $transaction->save();
+
     }
 
 
