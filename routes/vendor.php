e61aab73dd13517d866eebf598f9ade77dfc2042
TuanHung1995 <tuanhungg1995@gmail.com>
2025-02-07 23:32:30 +0700

    orders table for vendor

--- routes/vendor.php
+++ routes/vendor.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\ProductVariantController;
 use App\Http\Controllers\Backend\ProductVariantItemController;
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorOrderController;
 use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\VendorProductImageGallery;
 use App\Http\Controllers\Backend\VendorProductImageGalleryController;
@@ -47,3 +48,7 @@ Route::put('products-variant-item-update/{variantItemId}', [VendorProductVariant
 Route::delete('products-variant-item/{variantItemId}', [VendorProductVariantItemController::class, 'destroy'])->name('products-variant-item.destroy');
 
 Route::put('products-variant-item-status', [VendorProductVariantItemController::class, 'chageStatus'])->name('products-variant-item.chages-status');
+
+Route::get('orders', [VendorOrderController::class, 'index'])->name('orders.index');
+Route::get('orders/show/{id}', [VendorOrderController::class, 'show'])->name('orders.show');
+Route::get('orders/status/{id}', [VendorOrderController::class, 'orderStatus'])->name('orders.status');
