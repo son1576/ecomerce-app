c875829500d90212bce22cdb631eefbd108d2861
TuanHung1995 <tuanhungg1995@gmail.com>
2025-02-13 21:35:25 +0700

    product anf cart section banner

--- app/Http/Controllers/Backend/AdvertisementController.php
+++ app/Http/Controllers/Backend/AdvertisementController.php
@@ -25,11 +25,19 @@ class AdvertisementController extends Controller
         $homepage_section_banner_four = Advertisement::where('key', 'homepage_section_banner_four')->first();
         $homepage_section_banner_four = json_decode($homepage_section_banner_four->value);
 
+        $productpage_banner_section = Advertisement::where('key', 'productpage_banner_section')->first();
+        $productpage_banner_section = json_decode($productpage_banner_section?->value);
+
+        $cartpage_banner_section = Advertisement::where('key', 'cartpage_banner_section')->first();
+        $cartpage_banner_section = json_decode($cartpage_banner_section?->value);
+
         return view('admin.advertisement.index', compact([
             'homepage_section_banner_one',
             'homepage_section_banner_two',
             'homepage_section_banner_three',
-            'homepage_section_banner_four'
+            'homepage_section_banner_four',
+            'productpage_banner_section',
+            'cartpage_banner_section'
         ]));
     }
 
@@ -208,5 +216,89 @@ class AdvertisementController extends Controller
         toastr('Updated Successfully!', 'success', 'success');
 
         return redirect()->back();
+    }
+
+    public function productPageBanner(Request $request)
+    {
+        $request->validate([
+            'banner_image' => ['image'],
+            'banner_url' => ['required']
+           ]);
+
+            /** Handle the image upload */
+            $imagePath = $this->updateImage($request, 'banner_image', 'uploads');
+
+            $value = [
+                'banner_one' => [
+                    'banner_url' => $request->banner_url,
+                    'status' => $request->status == 'on' ? 1 : 0
+                ]
+            ];
+            if(!empty($imagePath)){
+                $value['banner_one']['banner_image'] = $imagePath;
+            }else {
+
+                $value['banner_one']['banner_image'] = $request->banner_old_image;
+            }
+
+            $value = json_encode($value);
+           Advertisement::updateOrCreate(
+                ['key' => 'productpage_banner_section'],
+                ['value' => $value]
+            );
+
+            toastr('Updated Successfully!', 'success', 'success');
+
+            return redirect()->back();
+
+    }
+
+    public function cartPageBanner(Request $request)
+    {
+        $request->validate([
+            'banner_one_image' => ['image'],
+            'banner_one_url' => ['required'],
+            'banner_two_image' => ['image'],
+            'banner_two_url' => ['required']
+        ]);
+
+        /** Handle the image upload */
+        $imagePath = $this->updateImage($request, 'banner_one_image', 'uploads');
+        $imagePathTwo = $this->updateImage($request, 'banner_two_image', 'uploads');
+
+
+        $value = [
+            'banner_one' => [
+                'banner_url' => $request->banner_one_url,
+                'status' => $request->banner_one_status == 'on' ? 1 : 0
+            ],
+            'banner_two' => [
+                'banner_url' => $request->banner_two_url,
+                'status' => $request->banner_two_status == 'on' ? 1 : 0
+            ]
+        ];
+        if(!empty($imagePath)){
+            $value['banner_one']['banner_image'] = $imagePath;
+        }else {
+
+            $value['banner_one']['banner_image'] = $request->banner_one_old_image;
+        }
+        if(!empty($imagePathTwo)){
+            $value['banner_two']['banner_image'] = $imagePathTwo;
+        }else {
+
+            $value['banner_two']['banner_image'] = $request->banner_two_old_image;
+        }
+
+        $value = json_encode($value);
+        Advertisement::updateOrCreate(
+            ['key' => 'cartpage_banner_section'],
+            ['value' => $value]
+        );
+
+        toastr('Updated Successfully!', 'success', 'success');
+
+        return redirect()->back();
+
     }
 }
