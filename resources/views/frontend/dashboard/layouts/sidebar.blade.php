7313708e6e839d9903ed44d5b47c8e9ff65fbb34
TuanHung1995 <tuanhungg1995@gmail.com>
2025-02-14 16:03:32 +0700

    modify sidebar

--- resources/views/frontend/dashboard/layouts/sidebar.blade.php
+++ resources/views/frontend/dashboard/layouts/sidebar.blade.php
@@ -6,16 +6,22 @@
     <a href="dsahboard.html" class="dash_logo"><img src="images/logo.png" alt="logo" class="img-fluid"></a>
     <ul class="dashboard_link">
         <li><a class="active" href="{{ route('user.dashboard') }}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
+        <li><a class="" href="{{ url('/') }}"><i class="fas fa-home"></i>Go To Home Page</a></li>
+
+        @if (auth()->user()->role === 'vendor')
+            <li><a class="{{ setActive(['vendor.dashbaord']) }}" href="{{ route('vendor.dashbaord') }}"><i
+                        class="fas fa-tachometer"></i>Go to Vendor Dashboard</a></li>
+        @endif
         <li><a class="{{ setActive(['user.orders.*']) }}" href="{{ route('user.orders.index') }}"><i
                     class="fas fa-list-ul"></i> Orders</a></li>
-        <li><a href="dsahboard_download.html"><i class="far fa-cloud-download-alt"></i> Downloads</a></li>
         <li><a href="{{ route('user.review.index') }}"><i class="far fa-star"></i> Reviews</a></li>
         <li><a href="dsahboard_wishlist.html"><i class="far fa-heart"></i> Wishlist</a></li>
         <li><a href="{{ route('user.profile') }}"><i class="far fa-user"></i> My Profile</a></li>
         <li><a href="{{ route('user.address.index') }}"><i class="fal fa-gift-card"></i> Addresses</a></li>
         @if (auth()->user()->role !== 'vendor')
-            <li><a class="{{ setActive(['user.vendor-request.*']) }}" href="{{ route('user.vendor-request.index') }}"><i
-                        class="far fa-user"></i> Request to be vendor</a></li>
+            <li><a class="{{ setActive(['user.vendor-request.*']) }}"
+                    href="{{ route('user.vendor-request.index') }}"><i class="far fa-user"></i> Request to be
+                    vendor</a></li>
         @endif
         <li>
             <form method="POST" action="{{ route('logout') }}">
